149c155d42144d8d87cb4d8ff6a6db921075d099
Hadouin Leroy <hadouinl@gmail.com>
2023-02-03 14:24:17 +0100

    add font

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,26 +2,55 @@ import Image from "next/image";
 import { api, RouterOutputs } from "../utils/api";
 import { CreateTweet } from "./CreateTweet";
 
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "1m",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dd",
+    M: "1M",
+    MM: "%dM",
+    y: "1y",
+    yy: "%dy",
+  },
+});
+
 function Tweet({
   tweet,
 }: {
   tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
 }) {
   return (
-    <div className="mb-4 flex">
-      {tweet.author.image && (
-        <Image
-          src={tweet.author.image}
-          alt={`${tweet.author.name} profile pic`}
-          width={48}
-          height={48}
-          className="rounded-full"
-        />
-      )}
+    <div className="mb-7 flex">
       <div>
-        <p>
-          {tweet.author.name} - {new Date(tweet.createdAt).toISOString()}
-        </p>
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile pic`}
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        )}
+      </div>
+      <div className="ml-2 flex flex-col">
+        <div className="flex flex-row items-center">
+          <p className="font-bold">{tweet.author.name}</p>
+          <p className="pl-1 text-xs text-gray-500">
+            {dayjs(tweet.createdAt).fromNow()}
+          </p>
+        </div>
         <p>{tweet.text}</p>
       </div>
     </div>
@@ -34,9 +63,6 @@ export function Timeline() {
   return (
     <div>
       <CreateTweet />
-
-      {JSON.stringify(data)}
-
       {data?.tweets.map((tweet) => {
         return <Tweet key={tweet.id} tweet={tweet} />;
       })}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#4A86E8",
+      },
+      fontFamily: {
+        sans: ["archia", ...defaultTheme.fontFamily.sans],
       },
     },
   },
